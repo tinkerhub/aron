502ccd6de24bf02e158479f37ff58346bd701eb6
Glitch (hello-express) <none>
2020-07-16 09:40:52 +0000

    🐥🚨 Checkpoint ./vue.config.js:3168504/117 ./package.json:3168504/1849 ./src/router/index.js:3168504/556 ./src/App.vue:3168504/5577 ./src/views/Home.vue:3168504/1021

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img src="https://cdn.khaleelgibran.com/img/donut.png" alt="Khaleel Gibran's blue donut" id="donut">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
+
+<style scoped>
+
+  #donut {
+    margin-top: 100px;
+    height: 160px;
+    border-radius: 80px;
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+  }
+  
+</style>
